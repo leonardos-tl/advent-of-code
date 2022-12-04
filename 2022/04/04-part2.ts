cbb4ad9386be510089c20c78642b1945f3bc9e21
Leo Sammarco <leonardo.sammarco@thetrainline.com>
2022-12-04 11:57:32 +0000

    day 4 part 2 implementation

--- 2022/04/04-part2.ts
+++ 2022/04/04-part2.ts
@@ -1,26 +1,24 @@
 import * as fs from "fs";
 
-const rucksacks = fs.readFileSync(__dirname + "/data.txt", {
+const choreList = fs.readFileSync(__dirname + "/data.txt", {
   encoding: "utf8",
   flag: "r",
 });
 
-const rucksackArray = rucksacks.split(/\r?\n/).filter((l) => !!l);
+const numListOverlapsSubList = (fl: string[], sl: string[]) =>
+  (Number(fl[0]) >= Number(sl[0]) && Number(fl[0]) <= Number(sl[1])) ||
+  (Number(fl[1]) >= Number(sl[0]) && Number(fl[1]) <= Number(sl[1]));
 
-let total = 0;
+const sumOfOverlappingChores = choreList
+  .split(/\r?\n/)
+  .filter((l) => !!l)
+  .map((cl) => cl.split(","))
+  .reduce((p, clp) => {
+    const fe = clp[0].split("-");
+    const se = clp[1].split("-");
+    return numListOverlapsSubList(fe, se) || numListOverlapsSubList(se, fe)
+      ? p + 1
+      : p;
+  }, 0);
 
-for (let i = 0; i < rucksackArray.length; i += 3) {
-  const chunk = rucksackArray.slice(i, i + 3);
-
-  chunk[0]
-    .split("")
-    .filter((l, i, s) => s.indexOf(l) === i)
-    .forEach((l) => {
-      if (chunk.every((cs) => cs.indexOf(l) > -1)) {
-        const asciiCode = l.charCodeAt(0);
-        total += asciiCode > 96 ? asciiCode - 96 : asciiCode - 38;
-      }
-    });
-}
-
-console.log(total);
+console.log(sumOfOverlappingChores);
